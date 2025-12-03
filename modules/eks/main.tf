41caca04567d413b7fb2cb23102848b85509f010
Shane Grigsby <refuge@rocktalus.com>
2025-12-02 16:38:38 -0800

    removing ancient and buggy IRSA module for proper auth via Pod Identity

--- modules/eks/main.tf
+++ modules/eks/main.tf
@@ -658,7 +658,7 @@ resource "aws_eks_addon" "ebs_csi_driver" {
   tags = var.tags
 
   depends_on = [
-    aws_eks_node_group.main,
+    aws_eks_pod_identity_association.ebs_csi_driver,
     aws_eks_node_group.system
   ]
 }
@@ -671,8 +671,45 @@ resource "aws_eks_addon" "pod_identity" {
   tags = var.tags
 
   depends_on = [
-    aws_eks_node_group.main,
     aws_eks_node_group.system
+  ]
+}
+
+# EBS CSI Driver IAM Role for Pod Identity
+resource "aws_iam_role" "ebs_csi_driver" {
+  name = "${var.cluster_name}-ebs-csi-driver"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Principal = {
+        Service = "pods.eks.amazonaws.com"
+      }
+      Action = [
+        "sts:AssumeRole",
+        "sts:TagSession"
+      ]
+    }]
+  })
+
+  tags = var.tags
+}
+
+resource "aws_iam_role_policy_attachment" "ebs_csi_driver" {
+  role       = aws_iam_role.ebs_csi_driver.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
+}
+
+# EBS CSI Driver Pod Identity Association
+resource "aws_eks_pod_identity_association" "ebs_csi_driver" {
+  cluster_name    = aws_eks_cluster.main.name
+  namespace       = "kube-system"
+  service_account = "ebs-csi-controller-sa"
+  role_arn        = aws_iam_role.ebs_csi_driver.arn
+
+  depends_on = [
+    aws_eks_addon.pod_identity
   ]
 }
 
